14e537b9775a8db7b5dcd72ec15a67c7266d5030
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-09-15 16:09:35 +0300

    on update and authors

--- Blazor_Books/Models/Books.cs
+++ Blazor_Books/Models/Books.cs
@@ -1,10 +1,24 @@
-﻿namespace Blazor_Books.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Books.Models
 {
     public class Books
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; } = string.Empty;
+        [Required]
         public string Description { get; set; } = string.Empty;
-        public string Author { get; set; } = string.Empty;
+        [Required]
+        public List<Author> Author { get; set; } = new List<Author>();
+
     }
+
+    //dto for author
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+    }   
 }
