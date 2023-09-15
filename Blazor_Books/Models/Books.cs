23a1503fc4e129669751b480c9deef1c1a353c2f
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-09-15 11:54:01 +0300

    first commit

---
+++ Blazor_Books/Models/Books.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor_Books.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+    }
+}
